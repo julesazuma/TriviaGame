29d73f57c9e5596f83809da5689ca9affcb52b35
PANDAmonium <jazuma86@yahoo.com>
2018-09-13 16:14:03 -0700

    created a object that holds all questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,67 @@
-    //create an object that holds all questions and answer information
+$(document).ready(function() {
 
+    //create an object that holds all questions and answer information
+    var questions = {
+        zero: {
+            question: ["What does Atari mean?"],
+            answers: ["Success", "Fun", "Attach", "Joy"],
+            correct: "Success"
+        },
+        one: {
+             question: "What does Mario jump on after completing a level?",
+            answers: ["Mystery Block", "Flag Pole", "Coin Block", "Pyramid"],
+            correct: "Flag Pole"
+        },
+    
+          two: {
+         	question: "What does NES stand for?",
+            answers: ["Nintendo Everyday System", "Nintendo Evolve System", "Nintendo Entertainment System", "Nintendo's Early System"],
+            correct: "Nintendo Entertainment System"
+        },
+    
+          three: {
+             question: "In what year was Playstation released?",
+            answers: ["1993", "1994", "1995", "1996"],
+            correct: "1994"
+        },
+    
+          four: {
+             question: "What is the name of the princess whom Mario repeatedly stops Bowser from kidnapping?",
+            answers: ["Princess Apple", "Princess Berry", "Princess Pie", "Princess Peach"],
+            correct: "Princess Peach"
+        },
+    
+          five: {
+             question: "What is the all-time highest grossing video game?",
+            answers: ["Grand Theft Auto", "Call of Duty", "World of Warcraft", "Fortnite"],
+            correct: "World of Warcraft"
+        },
+    
+          six: {
+             question: "What year was Sega Genesis released in North America?",
+            answers: ["1987", "1988", "1989", "1990"],
+            correct: "1989"
+        },
+    
+          seven: {
+             question: "What was Nintendo's first video game?",
+            answers: ["EVR Race", "Donkey Kong", "Mario", "Kung Fu"],
+            correct: "EVR Race"
+        },
+    
+          eight: {
+             question: "What was the Atari 2600's best selling game?",
+            answers: ["Asteroids", "Pac-Man", "Space Invaders", "Pong"],
+            correct: "Pac-Man"
+        },
+    
+          nine: {
+             question: "Approximately how many units of the first Xbox did Microsoft sell?",
+            answers: ["13 million", "24 million", "53 million", "77milion"],
+            correct: "24 million"
+        },
+    };
+    
     //create divs to contain information
 
     //create object keys to return the questions in order
@@ -15,5 +77,5 @@
     //create a function that shows the time remaining at the top of each question
 
     //create a function for a timer for the message after choosing an answer
-
-    //display the final score after 'checkIfLast' returns true
+ 
+    //display the final score after 'checkIfLast' returns 
