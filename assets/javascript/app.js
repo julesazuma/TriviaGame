4b8256429c6f4bbec78db6d9d87aefeabe926122
PANDAmonium <jazuma86@yahoo.com>
2018-09-13 12:25:06 -0700

    created html and css style sheet, also pseudo coded javascript page

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+    //create an object that holds all questions and answer information
+
+    //create divs to contain information
+
+    //create object keys to return the questions in order
+
+    //create a function with reset and game setup
+
+    //create a function to begin showing questions and messages that follow
+
+     //create a if then statement: if the question is right show you're right, if wrong show oops! the correct answer was"..."
+
+    //check to see if it is the last question: if it is show the score
+
+    //create a function that shows the time remaining at the top of each question
+
+    //create a function for a timer for the message after choosing an answer
+
+    //display the final score after 'checkIfLast' returns true
